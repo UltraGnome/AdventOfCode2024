95626268521bf74b04e079d483156ec91a49d6c6
UltraGnome <ultragnomeultra@duck.com>
2024-12-03 11:45:21 -0700

    day 2

--- Day2/01.go
+++ Day2/01.go
@@ -3,9 +3,7 @@ package main
 import (
 	"embed"
 	_ "embed"
-	"fmt"
 	"github.com/UltraGnome/AdventOfCode2024/pkg/math"
-	"sort"
 	"strconv"
 	"strings"
 
@@ -13,7 +11,7 @@ import (
 	"github.com/UltraGnome/AdventOfCode2024/pkg/utils"
 )
 
-//go:embed test1.txt
+//go:embed input.txt
 var input string
 
 //go:embed test*.txt
@@ -21,7 +19,7 @@ var tests embed.FS
 
 func main() {
 	h := harness.New(solve, input, tests)
-	h.Expect(1, 11)
+	h.Expect(1, 2)
 	h.Solve()
 }
 
@@ -30,25 +28,65 @@ func solve(input string) int {
 
 	var total int = 0
 	for _, line := range s {
-		pair := strings.Split(line, "   ")
-		n, err := strconv.Atoi(pair[0])
-		if err == nil {
+		reportStrings := strings.Split(line, " ")
+		reportInts := make([]int, len(reportStrings))
+		for i, report := range reportStrings {
+			reportInts[i], _ = strconv.Atoi(report)
+
 		}
-		left = append(left, int64(n))
-
-		m, e := strconv.Atoi(pair[1])
-		if e == nil {
+		if EvaluateReport(reportInts) {
+			total++
 		}
-		right = append(right, int64(m))
-	}
-	sort.Slice(left, func(i, j int) bool { return left[i] < left[j] })
 
-	sort.Slice(right, func(i, j int) bool { return right[i] < right[j] })
-
-	for i := 0; i < len(left); i++ {
-		fmt.Println("abs diff:" + strconv.FormatInt(math.Abs(left[i]-right[i]), 10))
-		total += math.Abs(left[i] - right[i])
 	}
-	fmt.Println("total is: " + strconv.FormatInt(total, 10))
+
 	return total
+}
+
+// The levels are either all increasing or all decreasing.
+// Any two adjacent levels differ by at least one and at most three.
+func EvaluateReport(report []int) bool {
+	levelDirection := "" //down,up
+	safe := true
+
+	safe, levelDirection = initializeReport(report, safe, levelDirection)
+
+	if !safe {
+		return safe
+	}
+
+	for i := 0; i < len(report); i++ {
+		if i == len(report)-1 {
+			break
+		}
+		if report[i] == report[i+1] {
+			safe = false
+			return safe
+		} else if levelDirection == "up" && report[i] > report[i+1] {
+			safe = false
+			return safe
+		} else if levelDirection == "down" && report[i] < report[i+1] {
+			safe = false
+			return safe
+		}
+		if math.Abs(report[i]-report[i+1]) > 3 {
+			safe = false
+			return safe
+		}
+	}
+	return safe
+
+}
+
+func initializeReport(report []int, safe bool, levelDirection string) (bool, string) {
+	if report[0] == report[1] {
+		safe = false
+		return false, ""
+	}
+	if report[0] > report[1] {
+		levelDirection = "down"
+	} else if report[0] < report[1] {
+		levelDirection = "up"
+	}
+	return safe, levelDirection
 }
--- Day2/02.go
+++ Day2/02.go
@@ -2,67 +2,51 @@ package main
 
 import (
 	"embed"
-	_ "embed"
-	"fmt"
-	"strconv"
-	"strings"
-
 	"github.com/UltraGnome/AdventOfCode2024/pkg/harness"
-	"github.com/UltraGnome/AdventOfCode2024/pkg/utils"
+	"github.com/UltraGnome/AdventOfCode2024/pkg/math"
+	"slices"
 )
+
+func solve(input [][]int) int {
+	safeReports := 0
+
+	for _, ints := range input {
+
+		for i := range ints {
+			if isSafe(slices.Concat(ints[:i], ints[i+1:])) {
+				safeReports++
+				break
+			}
+		}
+	}
+
+	return safeReports
+}
+
+func isSafe(ints []int) bool {
+	upOrDown := ints[1] > ints[0]
+	for i := 1; i < len(ints); i++ {
+		if ints[i] == ints[i-1] {
+			return false
+		}
+		if upOrDown != (ints[i] > ints[i-1]) {
+			return false
+		}
+		if math.Abs(ints[i]-ints[i-1]) > 3 {
+			return false
+		}
+	}
+	return true
+}
+
+func main() {
+	h := harness.New(solve, input, tests, harness.SplitNewlinesWithInts())
+	h.Tester.Expect(1, 4)
+	h.Run()
+}
 
 //go:embed input.txt
 var input string
 
 //go:embed test*.txt
 var tests embed.FS
-
-func main() {
-	h := harness.New(solve, input, tests)
-	//h.Expect(1, 31)
-	h.Solve()
-}
-
-func solve(input string) int64 {
-	s := utils.ParseInput(input)
-
-	left := []int64{}
-	right := []int64{}
-
-	var total int64 = 0
-	for _, line := range s {
-		pair := strings.Split(line, "   ")
-		n, err := strconv.Atoi(pair[0])
-		if err == nil {
-		}
-		left = append(left, int64(n))
-
-		m, e := strconv.Atoi(pair[1])
-		if e == nil {
-		}
-		right = append(right, int64(m))
-	}
-	//sort.Slice(left, func(i, j int) bool { return left[i] < left[j] })
-
-	//sort.Slice(right, func(i, j int) bool { return right[i] < right[j] })
-
-	itemCountmap := map[int64]int64{}
-
-	for _, nums := range left {
-		itemCountmap[nums] = 0
-	}
-	for _, num := range right {
-		if _, ok := itemCountmap[num]; ok {
-			itemCountmap[num]++
-		}
-	}
-	for key, v := range itemCountmap {
-
-		total = total + (key * v)
-
-	}
-
-	fmt.Println(total)
-
-	return total
-}
